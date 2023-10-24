97c5f23d82b00532812942fcf5865410bd7ada80
moxey.eth <jakemoxey@gmail.com>
2023-10-24 16:52:38 +1100

    wip: tweaks

--- src/app/components/CodeTitle.tsx
+++ src/app/components/CodeTitle.tsx
@@ -8,9 +8,9 @@ export function CodeTitle({ children, ...props }: { children: string }) {
       <div style={{ display: 'flex', alignItems: 'center', gap: 6 }}>
         {language === 'bash' ? (
           <Terminal width={14} height={14} style={{ marginTop: 3 }} />
-        ) : (
+        ) : children.match(/\.(.*)$/) ? (
           <File width={14} height={14} style={{ marginTop: 1 }} />
-        )}
+        ) : null}
         {children}
       </div>
     </div>
