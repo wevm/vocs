a59c13919bcbdb2af4e7285c9b320cfa32f08f60
moxey.eth <jakemoxey@gmail.com>
2023-10-24 18:06:28 +1100

    wip: vendor chunks

--- src/vite/vite.config.ts
+++ src/vite/vite.config.ts
@@ -1,5 +1,5 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
 
 import { css } from './plugins/css.js'
 import { mdx } from './plugins/mdx.js'
@@ -7,7 +7,7 @@ import { root } from './plugins/root.js'
 import { routes } from './plugins/routes.js'
 
 export default defineConfig({
-  plugins: [react(), css(), mdx(), routes(), root()],
+  plugins: [splitVendorChunkPlugin(), react(), css(), mdx(), routes(), root()],
   server: {
     fs: {
       allow: ['..'],
