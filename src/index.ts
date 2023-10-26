f31b3259ae7a325fe7a4b2b0ec162fb03d9eaabc
moxey.eth <jakemoxey@gmail.com>
2023-10-26 14:13:29 +1100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { default as h1Styles } from './app/components/mdx/H1.module.css'
-
 export { build } from './vite/build.js'
 export { createDevServer } from './vite/dev-server.js'
 export { preview } from './vite/preview.js'
