202a88f93b131859c352e9f837f90bd13427ece4
moxey.eth <jakemoxey@gmail.com>
2023-12-07 10:09:45 +1100

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,20 @@
 export { defineConfig } from './config.js'
+export type {
+  ColorScheme,
+  Config,
+  EditLink,
+  Font,
+  IconUrl,
+  LogoUrl,
+  Sidebar,
+  SidebarItem,
+  SocialItem,
+  SocialType,
+  Socials,
+  Theme,
+  TopNav,
+  TopNavItem,
+} from './config.js'
 
 export { build } from './vite/build.js'
 export { createDevServer } from './vite/devServer.js'
