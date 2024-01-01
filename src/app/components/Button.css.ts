6242e298f83626ac40f750104bfea64e48212442
moxey.eth <jakemoxey@gmail.com>
2024-01-02 09:24:28 +1100

    fix: textwrap

--- src/app/components/Button.css.ts
+++ src/app/components/Button.css.ts
@@ -24,6 +24,8 @@ export const button = style(
         background: primitiveColorVars.background3,
       },
     },
+    // @ts-expect-error
+    textWrap: 'nowrap',
     width: 'fit-content',
   },
   'button',
