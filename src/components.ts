1380e75cf120ee2ce00dea4043e9ec6c0bfeee97
Laurence Bugasu Ininda <laudebugs@gmail.com>
2024-02-16 06:43:58 -0500

    feat: expose mdx components for imports

--- src/components.ts
+++ src/components.ts
@@ -7,3 +7,5 @@ export { Raw } from './app/components/Raw.js'
 export { Sponsors } from './app/components/Sponsors.js'
 export { Steps, type StepsProps } from './app/components/Steps.js'
 export { Step, type StepProps } from './app/components/Step.js'
+
+export { components as MDXComponents } from './app/components/mdx/index.js'
