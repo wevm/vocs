8b83ae26bb327ac84784327fd04d261297b202a3
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-03-03 20:43:55 +0800

    feat: add compatibleAbsolutePath to format absolute path

--- src/vite/utils/paths.ts
+++ src/vite/utils/paths.ts
@@ -6,3 +6,9 @@ export const isWin32 = platform() === 'win32'
 export function getFsPath(path: string) {
   return `/@fs${padStartSlash(path, false)}`
 }
+
+// Only file and data URLs are supported by the default ESM loader. 
+// On Windows, absolute paths must be valid `file://` URLs.
+export function compatibleAbsolutePath(path: string) {
+  return isWin32 ? `file://${path}` : path
+}
