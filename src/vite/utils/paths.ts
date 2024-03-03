fb41037469dfbbcf982c80c8f1c13ab6e67826d0
chengpeiquan <chengpeiquan@chengpeiquan.com>
2024-03-03 20:59:44 +0800

    chore: typo

--- src/vite/utils/paths.ts
+++ src/vite/utils/paths.ts
@@ -7,7 +7,7 @@ export function getFsPath(path: string) {
   return `/@fs${padStartSlash(path, false)}`
 }
 
-// Only file and data URLs are supported by the default ESM loader. 
+// Only file and data URLs are supported by the default ESM loader.
 // On Windows, absolute paths must be valid `file://` URLs.
 export function compatibleAbsolutePath(path: string) {
   return isWin32 ? `file://${path}` : path
