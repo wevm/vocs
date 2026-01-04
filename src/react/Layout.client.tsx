5bdeea4d54d0a0c95d4968a77d6b6d7033bd1baf
jxom <7336481+jxom@users.noreply.github.com>
2026-01-04 17:51:49 +1100

    wip: mobile layout

--- src/react/Layout.client.tsx
+++ src/react/Layout.client.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { cx } from 'cva'
 import * as React from 'react'
 import { Link } from 'waku'
 import * as AskAi from './internal/AskAi.js'
@@ -7,19 +8,21 @@ import * as Search from './internal/Search.js'
 import * as Sidebar from './internal/Sidebar.js'
 import * as TopNav from './internal/TopNav.js'
 import { useConfig } from './useConfig.js'
+import { useScrollDirection } from './useScrollDirection.js'
 
 export function Main(props: Main.Props) {
   const { children } = props
 
   const sidebarScrollRef = React.useRef<HTMLDivElement>(null)
+  const scrollDirection = useScrollDirection({ deltaY: 200 })
 
   return (
     <div>
       <div
-        className="vocs:flex vocs:justify-end vocs:fixed vocs:h-topNav vocs:w-logo vocs:min-w-fit vocs:max-lg:w-fit vocs:z-20"
+        className="vocs:flex vocs:max-lg:hidden vocs:justify-end vocs:fixed vocs:h-topNav vocs:w-logo vocs:min-w-fit vocs:max-lg:w-fit vocs:z-20"
         data-v-gutter-logo
       >
-        <div className="vocs:w-sidebar vocs:px-sidebar-px vocs:min-w-fit" data-v-logo>
+        <div className="vocs:w-sidebar vocs:px-sidebar-px vocs:py-3 vocs:min-w-fit" data-v-logo>
           <Link to="/">
             <Logo />
           </Link>
@@ -58,22 +61,32 @@ export function Main(props: Main.Props) {
       </div>
 
       <div
-        className="vocs:bg-primary vocs:fixed vocs:flex vocs:justify-between vocs:lg:left-gutter vocs:w-[calc(100vw-var(--vocs-spacing-gutter))] vocs:pr-[calc(var(--vocs-spacing-gutter)-var(--vocs-spacing-sidebar)-(var(--vocs-spacing)*4))] vocs:h-topNav vocs:px-4 vocs:z-10"
+        className={cx(
+          'vocs:bg-primary vocs:fixed vocs:flex vocs:justify-between vocs:lg:left-gutter vocs:w-[calc(100vw-var(--vocs-spacing-gutter))] vocs:pr-[calc(var(--vocs-spacing-gutter)-var(--vocs-spacing-sidebar)-(var(--vocs-spacing)*4))] vocs:h-topNav vocs:px-4 vocs:z-10 vocs:max-lg:w-full vocs:max-lg:left-0 vocs:max-lg:pr-0 vocs:max-lg:transition-transform vocs:max-lg:duration-300 vocs:max-lg:will-change-transform vocs:max-md:border-b vocs:max-md:border-primary',
+          scrollDirection === 'down' && 'vocs:max-md:-translate-y-full',
+        )}
         data-v-gutter-top
       >
-        <div className="vocs:flex vocs:gap-2 vocs:max-lg:hidden vocs:h-full vocs:py-2 vocs:-ml-4">
-          <div className="vocs:w-[240px]">
+        <div className="vocs:flex vocs:gap-2 vocs:h-full vocs:py-2 vocs:lg:-ml-7">
+          <Link className="vocs:lg:hidden vocs:py-0.5" to="/">
+            <Logo />
+          </Link>
+
+          <div className="vocs:w-1" />
+
+          <div className="vocs:max-lg:w-[180px] vocs:w-[240px] vocs:max-md:hidden">
             <Search.Search />
           </div>
-          <div className="vocs:w-[120px]">
+
+          <div className="vocs:w-[120px] vocs:max-lg:hidden">
             <AskAi.AskAi />
           </div>
         </div>
 
-        <TopNav.TopNav className="vocs:max-lg:hidden" />
+        <TopNav.TopNav className="vocs:max-md:hidden vocs:px-2" />
       </div>
 
-      <div className="vocs:fixed vocs:bg-surface vocs:lg:rounded-tl-2xl vocs:lg:border-l vocs:border-t vocs:border-primary vocs:w-[calc(100vw-var(--vocs-spacing-gutter))] vocs:h-full vocs:max-lg:w-full vocs:top-topNav vocs:lg:translate-x-gutter" />
+      <div className="vocs:fixed vocs:bg-surface vocs:lg:rounded-tl-2xl vocs:lg:border-l vocs:border-t vocs:border-primary vocs:w-[calc(100vw-var(--vocs-spacing-gutter))] vocs:h-full vocs:max-lg:w-full vocs:top-topNav vocs:max-md:top-0 vocs:lg:translate-x-gutter" />
 
       <main className="vocs:isolate vocs:pt-topNav vocs:w-full vocs:h-full" data-v-main>
         <article
@@ -104,7 +117,7 @@ export function Logo() {
   const { logoUrl, title } = useConfig()
 
   return (
-    <div className="vocs:flex vocs:items-center vocs:h-full vocs:py-3" data-v-logo>
+    <div className="vocs:flex vocs:items-center vocs:h-full" data-v-logo>
       {(() => {
         if (!logoUrl)
           return (
