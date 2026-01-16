e1011ba6efe6e571db13e45d25c5cc4ef9dae587
jxom <7336481+jxom@users.noreply.github.com>
2026-01-16 00:38:14 +0000

    chore: catalog

--- site/src/pages.gen.ts
+++ site/src/pages.gen.ts
@@ -1,17 +1,20 @@
 // deno-fmt-ignore-file
 // biome-ignore format: generated types do not need formatting
 // prettier-ignore
-import type { PathsForPages } from 'waku/router';
+import type { PathsForPages, GetConfigResponse } from 'waku/router';
+
 
 // prettier-ignore
-type Page = { path: '/'; render: 'static' }
+type Page =
+| { path: '/changelog'; render: 'static' }
+| { path: '/'; render: 'static' };
 
 // prettier-ignore
 declare module 'waku/router' {
   interface RouteConfig {
-    paths: PathsForPages<Page>
+    paths: PathsForPages<Page>;
   }
   interface CreatePagesConfig {
-    pages: Page
+    pages: Page;
   }
 }
